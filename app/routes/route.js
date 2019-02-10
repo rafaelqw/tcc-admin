c327678f4ba7c9a3a6daa7d090bbe6bd19051660
kenzo13 <raphaelkenzo2586@gmail.com>
2019-02-10 16:38:13 -0200

    Tela de Clientes

--- app/routes/route.js
+++ app/routes/route.js
@@ -12,5 +12,14 @@ app.config(function($routeProvider) {
 			controller : "EmpreendimentoCtrl"
 		})
 
+		.when("/usuario-new", {
+			templateUrl : "app/views/usuario-new.html",
+			controller : "usuarioCtrl"
+		})
+		.when("/usuario-list", {
+			templateUrl : "app/views/usuario-list.html",
+			controller : "EmpreendimentoCtrl"
+		})
+
 		.otherwise ({ redirectTo: '/' });
 });
